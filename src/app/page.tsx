44a01c70f82080e4ca0c666bc812f33df4a0f21c
achmadakif <83754562+AchmadAkif@users.noreply.github.com>
2025-10-08 19:46:08 +0700

    refactor: move Header component declaration into layout.tsx instead of declaring on each page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import Header from "./components/Header";
 import Hero from "./components/Hero";
 
 export default function Home() {
-  return (
-    <main>
-      <Header />
-      <Hero />
-    </main>
-  );
+  return <Hero />;
 }

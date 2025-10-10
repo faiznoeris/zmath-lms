45e3817ccd3ee1c0e3195847c6add0843edc0c3f
achmadakif <83754562+AchmadAkif@users.noreply.github.com>
2025-10-10 18:42:10 +0700

    fix: allow access to the register page without user authentication

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -42,6 +42,7 @@ export async function updateSession(request: NextRequest) {
   if (
     !user &&
     !request.nextUrl.pathname.startsWith("/login") &&
+    !request.nextUrl.pathname.startsWith("/register") &&
     !request.nextUrl.pathname.startsWith("/auth") &&
     !request.nextUrl.pathname.startsWith("/error")
   ) {

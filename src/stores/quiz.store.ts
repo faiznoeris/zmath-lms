d67d3458792e2ecbd6f43f7a8e65801b4cf855d6
achmadakif <83754562+AchmadAkif@users.noreply.github.com>
2025-11-06 01:58:28 +0700

    refactor: simplify quiz store state by removing unused properties and methods

--- src/stores/quiz.store.ts
+++ src/stores/quiz.store.ts
@@ -1,19 +1,11 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
-import { Quiz } from "../models/Quiz";
-import { Question } from "../models/Question";
-
-interface QuizWithQuestions extends Quiz {
-  queston: Question[];
-}
+import { QuizWithQuestions } from "../types";
 
 interface IQuizState {
   quiz: QuizWithQuestions | null;
-  numberOfQuestions: number;
   currentQuestionIndex: number;
   setQuiz: (quizData: QuizWithQuestions) => void;
-  setNumberOfQuestions: (totalQuestions: number) => void;
-  setCurrentQuestionIndex: (index: number) => void;
 }
 
 export const useQuizStore = create<IQuizState>()(
@@ -26,8 +18,5 @@ export const useQuizStore = create<IQuizState>()(
         quiz: quizData,
         currentQuestionIndex: 0,
       }),
-    setNumberOfQuestions: totalQuestions =>
-      set({ numberOfQuestions: totalQuestions }),
-    setCurrentQuestionIndex: index => set({ currentQuestionIndex: index }),
   }))
 );

279dbe4ee3fae6fb881a82a5315745f9b9798bdf
faiznoeris <m.faiznoeris@gmail.com>
2025-11-14 10:17:38 +0700

    fix: fix stringAvatar

--- src/utils/stringAvatar.ts
+++ src/utils/stringAvatar.ts
@@ -3,7 +3,7 @@ import { stringToColor } from "./stringToColor";
 export const stringAvatar = (name: string) => {
   const splitName = name.split(" ");
   const initialName = splitName
-    .map(word => word.split("")[0].toUpperCase())
+    .map(word => word.split("")[0]?.toUpperCase())
     .join("");
 
   return {
